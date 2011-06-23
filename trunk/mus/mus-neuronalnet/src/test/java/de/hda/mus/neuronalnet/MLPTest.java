50ea896b3739661b4054d6a607bd023698d02723
andreas@schaeffernet.de <andreas@schaeffernet.de@6fcac06b-e7a0-ee76-85e0-4c934b450df1>
2011-06-23 11:02:11 +0000

    tests

---
+++ trunk/mus/mus-neuronalnet/src/test/java/de/hda/mus/neuronalnet/MLPTest.java
@@ -0,0 +1,12 @@
+package de.hda.mus.neuronalnet;
+
+import org.junit.Test;
+
+public class MLPTest {
+
+	@Test
+	public void mlpTest() {
+		MLP mlp = new MLP(20,20,20);
+	}
+
+}
---
+++ trunk/mus/mus-neuronalnet/src/test/java/de/hda/mus/neuronalnet/NeuronTest.java
@@ -0,0 +1,11 @@
+package de.hda.mus.neuronalnet;
+
+import org.junit.Test;
+
+public class NeuronTest {
+
+	@Test
+	public void neuronTest() {
+		
+	}
+}
