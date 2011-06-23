6227802625de693133456a5edb6b350f6161c7e1
andreas@schaeffernet.de <andreas@schaeffernet.de@6fcac06b-e7a0-ee76-85e0-4c934b450df1>
2011-06-23 10:43:38 +0000

    + transfer functions

--- mus/mus-neuronalnet/src/main/java/de/hda/mus/neuronalnet/transferfunction/LinearFunction.java
+++ mus/mus-neuronalnet/src/main/java/de/hda/mus/neuronalnet/transferfunction/LinearFunction.java
@@ -4,9 +4,7 @@ public class LinearFunction implements TransferFunction {
 
 	@Override
 	public double proceedFunction(double input) {
-		// TODO Auto-generated method stub
-		// returns positiv and negetive values
-		return 0;
+		return input;
 	}
 
 }
