e85eebb85f019635d40f126c9bfcfe7edae5470a
andreas@schaeffernet.de <andreas@schaeffernet.de@6fcac06b-e7a0-ee76-85e0-4c934b450df1>
2011-06-24 09:32:02 +0000

    javadocs, cleanup, interfaces, constructors

--- trunk/mus/mus-neuronalnet/src/main/java/de/hda/mus/neuronalnet/InputNeuron.java
+++ trunk/mus/mus-neuronalnet/src/main/java/de/hda/mus/neuronalnet/InputNeuron.java
@@ -1,26 +1,64 @@
 package de.hda.mus.neuronalnet;
 
+import de.hda.mus.neuronalnet.transferfunction.LinearFunction;
 import de.hda.mus.neuronalnet.transferfunction.TransferFunction;
 
+/**
+ * InputNeuron is a specialized Neuron with an input value.
+ * 
+ * @author aschaeffer
+ * @author khalid
+ *
+ */
 public class InputNeuron extends Neuron {
-	
+
+	/**
+	 * The input value.
+	 */
 	private double value;
 
+	/**
+	 * Constructor for the input neuron.
+	 * @param value The initial input value.
+	 * @param transferFunction The transfer function.
+	 */
 	public InputNeuron(double value, TransferFunction transferFunction) {
 		super(transferFunction);
 		this.value = value;
 	}
 	
+	/**
+	 * Constructor for the input neuron using the linear transfer function.
+	 * 
+	 * @param value The initial input value.
+	 */
+	public InputNeuron(double value) {
+		super(new LinearFunction());
+		this.value = value;
+	}
+
+	/**
+	 * An simplified activation function for input neurons.
+	 * @return The activation of the input neuron (usally the input value).
+	 */
 	@Override
 	public double activation(){
 		System.out.println(this.getName() + " feuert: value=" + getTransferFunction().proceedFunction(value) + " (" + this.getValue() + ")");
 		return getTransferFunction().proceedFunction(value);
 	}
 	
+	/**
+	 * Get the input value.
+	 * @return The input values.
+	 */
 	public double getValue() {
 		return this.value;
 	}
 
+	/**
+	 * Setter for the input value.
+	 * @param value The new input value.
+	 */
 	public void setValue(double value) {
 		this.value = value;
 	}
